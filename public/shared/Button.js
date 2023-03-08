5ba9cb675a24b361d7fb60d7124135e6a36a563b
Jin Eu <a11718trannhuy@gmail.com>
2023-03-08 16:30:04 +0700

    fix something

--- public/shared/Button.js
+++ public/shared/Button.js
@@ -1,15 +1,18 @@
-function Button ({ icon, content, backgroundColor, hoverColor, onClick, rounded, iconColor, contentColor })
+import Image from "next/image"
+
+function Button ({ icon, content, backgroundColor, hoverColor, onClick, rounded, iconColor, contentColor, border })
 {
     return (
         <div 
             className={`w-full h-full flex items-center select-none cursor-pointer justify-center bg-[${backgroundColor}] rounded-[${rounded}]
                 ${hoverColor ? `hover:bg-[${hoverColor}]` : ""}
+                ${border ? "border-[1px] border-[#333]" : ""}
             `}
             onClick={onClick}
         >
-            { icon? (<div className={`text-${iconColor} mr-1`}>{icon}</div>) : (<></>)}
+            { icon? (<Image className="w-[24px] h-[24px]" src={icon} alt="img" />) : (<></>)}
             <div 
-                className={`text-${contentColor} text-[14px] font-[600] ${icon ? "ml-1" : ""}`}
+                className={`text-${contentColor} text-[14px] font-[600] ${icon ? "ml-2" : ""}`}
             >
                 {content}
             </div>
