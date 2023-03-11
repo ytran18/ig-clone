1766e008d9dc518a22c26c3be490dae70247ab59
Jin Eu <a11718trannhuy@gmail.com>
2023-03-11 21:49:29 +0700

    [wIp] sidebar responsive, no features

--- public/shared/Sidebar.js
+++ public/shared/Sidebar.js
@@ -1,33 +1,67 @@
 import Link from "next/link"
-import { IgSidebar, IgIcon, HomeIcon } from "../icons/icons"
+import { useEffect, useState } from "react"
+import { IgSidebar, IgIcon, HomeIcon, SearchIcon, Explore, ReelIcon, MessagesIcon, PostsIcon, NotificationIcon, MoreIcon, CreateIcon } from "../icons/icons"
 
 function Sidebar ()
 {
+    const [state, setState] = useState(0)
 
-    const handleZoomIcon = (id) =>
+    useEffect(() =>
     {
-        const icon = document.getElementById(id)
-        console.log(icon);
-    }
+        state === 1 ? console.log("search") : console.log("notifications")
+    },[state])
 
     return (
-        <div className="hidden md:flex md:flex-col md:justify-between md:items-center lg:flex lg:flex-col lg:justify-between lg:items-center md:w-[73px] lg:w-[245px] h-full border-r-[1px] border-[rgb(219,219,219)] py-8">
+        <div className={`hidden md:flex md:flex-col md:justify-between md:items-center lg:flex lg:flex-col lg:justify-between lg:items-center md:w-[73px] h-full border-r-[1px] border-[rgb(219,219,219)] py-8 ${state === 1 ? "lg:w-[73px]]" : `${state === 2 ? "lg:w-[73px]" : "lg:w-[245px]"}`}`}>
             <div className="flex flex-col w-full">
-                <Link className="md:hidden lg:flex justify-center mb-8" href="/" >
+                <Link className={`md:hidden lg:flex px-7 mb-8 ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`} href="/" >
                     {IgSidebar}
                 </Link>
-                <Link className="md:flex lg:hidden justify-center mb-8" href="/" >
+                <Link className={`md:flex justify-center mb-8 ${state === 1 ? "lg:flex" : `${state === 2 ? "lg:flex" : "lg:hidden"}`}`} href="/" >
                     {IgIcon}
                 </Link>
-                <div className="flex flex-col w-full items-center">
-                    <Link className="flex items-center justify-center lg:w-[220px] md:w-[48px] md:h-[48px] lg:h-[56px] hover:bg-[rgb(250,250,250)] rounded-[25px]" onMouseEnter={handleZoomIcon("home")} href="/">
-                        <div id="home" className="lg:mr-3 md:flex md:items-center md:justify-center transition-transform duration-300 transform">{HomeIcon}</div>
-                        <div className="font-[500] ml-3 md:hidden lg:flex lg:justify-center lg: items-center">Home</div>
+                <div className="flex flex-col w-full">
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{HomeIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Home</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{SearchIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Search</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{Explore}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Explore</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{ReelIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Reels</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{MessagesIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Messages</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{NotificationIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Notifications</div>
+                    </Link>
+
+                    <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                        <div className="lg:pl-2 flex md:justify-center md:items-center">{CreateIcon}</div>
+                        <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>Create</div>
                     </Link>
                 </div>  
             </div>
-            <div className="">
-                third
+            <div className="flex flex-col w-full">
+                <Link className="flex lg:mx-5 lg:h-[56px] md:h-[48px] lg:justify-start md:justify-center items-center hover:bg-[rgb(250,250,250)] rounded-[25px]" href="/">
+                    <div className="lg:pl-2 flex md:justify-center md:items-center">{MoreIcon}</div>
+                    <div className={`font-[500] ml-5 md:hidden lg:justify-center lg: items-center ${state === 1 ? "lg:hidden" : `${state === 2 ? "lg:hidden" : "lg:flex"}`}`}>More</div>
+                </Link>
             </div>
         </div>
     )
