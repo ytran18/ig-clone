fd892101829c330c2c792e5c9ad7c7cbf5d21b37
Jin Eu <a11718trannhuy@gmail.com>
2023-03-13 23:16:44 +0700

    addded icon

--- public/icons/icons.js
+++ public/icons/icons.js
@@ -29,6 +29,12 @@ const CloseIcon = (
     </svg>
 )
 
+const CloseIconSmall = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+)
+
 const HomeIcon = (
     <svg aria-label="Home" class="_ab6-" color="rgb(38,38,38)" fill="rgb(38,38,38)" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M9.005 16.545a2.997 2.997 0 0 1 2.997-2.997A2.997 2.997 0 0 1 15 16.545V22h7V11.543L12 2 2 11.543V22h7.005Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
 )
