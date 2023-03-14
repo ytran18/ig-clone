87146900221f024a89665d62547cbfeb1390b0a3
Jin Eu <a11718trannhuy@gmail.com>
2023-03-14 21:55:26 +0700

    config redux

---
+++ public/redux/selectors/userSelector.js
@@ -0,0 +1,9 @@
+export const selectUserReducer = (state) =>
+{
+    return state.userReducer
+}
+
+export const userPackageSelector = (state) =>
+{
+    return selectUserReducer(state).user
+}
--- public/redux/store.js
+++ public/redux/store.js
@@ -1,4 +1,20 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
+import { userReducer } from "./reducer"
 import thunk from "redux-thunk"
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({
+    userReducer: userReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk))
+
+export const persistor = persistStore(store)
