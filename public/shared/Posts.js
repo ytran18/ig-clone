7b83b4336acdc9f80978113f87145bd8d22845d2
vuhung1402 <badao867@gmail.com>
2023-03-14 16:25:18 +0700

    done content when don't have anything

--- public/shared/Posts.js
+++ public/shared/Posts.js
@@ -1,8 +1,11 @@
+import Image from "next/image"
+import Camera  from "../icons/camera.png"
 
 function Posts() {
     return (
         <div className="flex flex-col items-center justify-center h-[300px]">
             <div className="flex flex-col items-center justify-center">
+                <Image className="w-[50px] h-[50px] mb-[20px]" src={Camera} />
                 <div className="text-[32px] font-extrabold mb-[20px]">Share Photos</div>
                 <div className="mb-[10px]">When you share photos, they will appear on your profile.</div>
                 <div className="text-[#2997f6] cursor-pointer hover:text-black">Share your first photo</div>
--- public/shared/Tagged.js
+++ public/shared/Tagged.js
@@ -1,8 +1,11 @@
+import Image from "next/image"
+import Avatar  from "../icons/avatar.png"
 
 function Tagged() {
     return(
         <div className="flex flex-col items-center justify-center h-[300px]">
             <div className="flex flex-col items-center justify-center">
+                <Image className="w-[60px] h-[60px] mb-[20px]" src={Avatar} />
                 <div className="text-[32px] font-extrabold mb-[20px]">Photos of you</div>
                 <div className="mb-[10px]">When people tag you in photos, they'll appear here.</div>
             </div>
