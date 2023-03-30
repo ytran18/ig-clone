4db71210f80c14a14f04c45939404af1a9b52f41
Jin Eu <a11718trannhuy@gmail.com>
2023-03-30 13:01:35 +0700

    using props

--- public/shared/PostArea.js
+++ public/shared/PostArea.js
@@ -1,32 +1,7 @@
-// firebase
-import { ref, query, onValue } from "firebase/database"
-import { useEffect, useState } from "react"
-import { db } from "../../src/firebase"
-
 import Post from "./Post"
 
-function PostArea ()
+function PostArea ({ posts })
 {
-
-    // state
-    const [posts, setPosts] = useState([]) // state to store posts get from firebase realtime database
-
-    // query to get post from firebase realtime database
-    const getPost = query(ref(db,"posts/test"))
-
-    // get all posts from firebase realtime database
-    useEffect(() =>
-    {
-        onValue(getPost, (snapshot) =>
-        {
-            const value = snapshot.val()
-            if (value != null)
-            {
-                let postObject = Object.values(value)
-                setPosts(postObject)
-            }
-        })
-    },[])
 
     return (
         <div className="w-full h-full flex flex-col items-center">
