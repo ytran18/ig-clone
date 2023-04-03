d0cbd0472f3def4c900ea4bd2c71782d3a4754b2
Jin Eu <a11718trannhuy@gmail.com>
2023-04-03 12:52:12 +0700

    add dot icon

--- public/icons/icons.js
+++ public/icons/icons.js
@@ -197,12 +197,13 @@ const Plus = (
     </svg>
 )
 
-
+const Dot = (
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="currentColor" viewBox="0 0 16 16"> <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/> </svg>
+)
 
 export { IgLogo, FacebookIcon, SuccessIcon, FailIcon, CloseIcon, HomeIcon, Explore, ReelIcon, 
     MessagesIcon, NotificationIcon, CreateIcon, IgSidebar, IgIcon, EditIcon, PostsIcon, SavedIcon, 
     TaggedIcon, SearchIcon, MoreIcon, HomeSelectIcon, SearchSelectIcon, ExploreSelectIcon, ReelSelectIcon, 
     MessagesSelectIcon, NotificationSelectIcon, CreateSelectIcon, MoreSelectIcon, TimeIcon, SavedIconBig, 
     MoonIcon, MediaIcon, CloseIconSmall, LoveIcon, CommentIcon, MoreDotIcon, ArrowLeft, ArrowRight, ChevronLeft, ChevronRight, 
-    DocumentDuplicate, Plus
-}
+    DocumentDuplicate, Plus, Dot }
