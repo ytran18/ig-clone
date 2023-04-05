1265f7c8cf40c1f7327c5b98cda8edf7321d378c
vuhung1402 <badao867@gmail.com>
2023-04-05 21:20:50 +0700

    add PostPopUp

--- public/shared/Posts.js
+++ public/shared/Posts.js
@@ -7,19 +7,29 @@ import { db } from "../../src/firebase"
 import {onValue, ref, set } from "firebase/database"
 
 // icons/image
-import { CommentIcon, LoveIcon } from "../icons/icons"
+import { CommentIcon, LoveIcon, DocumentDuplicate } from "../icons/icons"
 import Image from "next/image"
 import Camera  from "../icons/camera.png"
 import CreatePost from "./CreatePost"
 import Loading from "./Loading"
 
+//component
+import PostPopUp from "../shared/PostPopUp"
+
 function Posts({ userData, posts }) {
     const [createPost, setCreatePost] = useState(false)
+    const [postPopUp, setPostPopUp] = useState(false)
 
     console.log(posts)
 
+    console.log(userData);
+
     const handleCreatePost = () => {
         setCreatePost(!createPost)
+    }
+
+    const handlePostPopUp = () => {
+        setPostPopUp(!postPopUp)
     }
 
     return (
@@ -57,16 +67,18 @@ function Posts({ userData, posts }) {
                                 </div>
                             ) :
                             (
-                                <div className="flex flex-wrap justify-between">
+                                <div className="flex flex-wrap">
                                 {
                                     posts.map( (post) => {
                                         return(
-                                            <div className="cursor-pointer select-none relative group mt-[10px] bg-slate-900 h-[300px] w-[300px] flex items-center justify-center">
-                                                <img
-                                                        className="max-h-[300px] max-w-[300px]"
-                                                        src={post.media[0].url}  
-                                                        alt = "y ngu"
-                                                />
+                                            <div onClick={handlePostPopUp} className="cursor-pointer select-none relative group mt-[10px] bg-slate-900 h-[300px] w-[300px] flex items-center justify-center mr-[10px]">
+                                                
+                                                { post.media[0].type == "img" ? 
+                                                    (<img className="max-h-[300px] max-w-[300px]" src={post.media[0].url} alt = "post"/>) : 
+
+                                                    (<video className="max-h-[300px] max-w-[300px]" src={post.media[0].url} alt = "post" autoPlay/>)
+                                                } 
+
                                                 <div className=" absolute group-hover:flex justify-evenly items-center inset-0 hidden bg-[rgba(35,35,35,0.16)]">
                                                     <div className="flex items-center text-white">
                                                         {CommentIcon} 
@@ -74,8 +86,17 @@ function Posts({ userData, posts }) {
                                                     </div>
                                                     <div className="flex items-center text-white">
                                                         {LoveIcon}
-                                                        <div className=" font-bold text-white">{post.likes == undefined ? "0" : `${post.comment.length}`}</div>
+                                                        <div className=" font-bold text-white">{post.likes == undefined ? "0" : `${post.likes.length}`}</div>
                                                     </div>
+                                                </div>
+                                                    
+                                                {   post.media.length > 1 &&
+                                                    (<div className=" absolute top-2 right-2 text-white">
+                                                        {DocumentDuplicate}
+                                                    </div>)
+                                                }
+                                                <div className={postPopUp ? "fixed w-screen h-screen top-0 left-0 bottom-0 right-0 bg-[rgba(19,18,18,0.71)] bg-opacity-90 flex justify-center items-center drop-shadow-2xl shadow-2xl" : "hidden"}>
+                                                    <PostPopUp caption={post.caption} createAt={post.createAt} amountOfLove={post.likes} owner={userData.userId} />
                                                 </div>
                                             </div>
                                         )
