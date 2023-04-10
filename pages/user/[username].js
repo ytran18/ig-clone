659081038c396419460c45c585b40f297bb6bed9
vuhung1402 <badao867@gmail.com>
2023-04-10 22:27:58 +0700

    add following

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -8,7 +8,7 @@ import { useRouter } from "next/router"
 import { EditIcon, PostsIcon, SavedIcon, TaggedIcon } from "../../public/icons/icons"
 
 //firebase
-import { ref as ref2, update, onValue} from "firebase/database"
+import { ref as ref2, update, onValue, set} from "firebase/database"
 import { getDownloadURL, ref, uploadBytes, } from "firebase/storage"
 import { storage,db } from "../../src/firebase"
 
@@ -21,6 +21,7 @@ import Tagged from "../../public/shared/Tagged"
 import FollowingPopUp from "../../public/shared/FollowingPopUp"
 import FollowersPopUp from "../../public/shared/FollowersPopUp"
 import Loading from "../../public/shared/Loading"
+import Reels from "../../public/shared/Reel"
 
 
 function AccountPage() {
@@ -94,6 +95,30 @@ function AccountPage() {
         return data1
     }
 
+    //get following 
+    const getFollowing = () => {
+        let following = []
+        onValue(ref2(db,'users/' + userData?.userId + '/following'), (snapshot) => {
+            snapshot.forEach( (childSnapshot) => {
+                following.push(childSnapshot.val())
+            })
+        })
+        return following
+    }
+
+    //check following
+    const isFollowing = (ohter) => {
+        const following = getFollowing()
+        for(let i = 0; i < following.length; i++){
+            if(following[i] == ohter?.userId){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    console.log(getFollowing());
+
     //get number of posts's user
     const numOfPost = () => {
         let total = 0
@@ -105,6 +130,12 @@ function AccountPage() {
     console.log(numOfPost())
 
     console.log("User: ",getUser());
+
+    const handleFollow = () => {
+        update(ref2(db, 'users/' + userData.userId),{
+            following: [otherUser?.userId]
+        })
+    }
      
     const handleEditPopUp = () => {
         setIsEdit(!isEdit)
@@ -174,9 +205,18 @@ function AccountPage() {
                                     (<div className="bg-[#efefef] mr-[10px] rounded py-[7px] px-[16px] font-semibold text-[14px]">
                                         Edit Profile 
                                     </div>) :
-                                    (<div className=" bg-sky-400 mr-[10px] rounded py-[7px] px-[16px] font-semibold text-[14px] cursor-pointer">
-                                        Follow
-                                    </div>)
+                                    ( isFollowing(otherUser) ? 
+                                        (
+                                            <div className="bg-[#efefef] mr-[10px] rounded py-[7px] px-[16px] font-semibold text-[14px]">
+                                                Following
+                                            </div>
+                                        ) :
+                                        (
+                                        <div onClick={handleFollow} className=" bg-sky-400 mr-[10px] rounded py-[7px] px-[16px] font-semibold text-[14px] cursor-pointer">
+                                            Follow
+                                        </div>
+                                        )
+                                    )
                                 }
                                 <div 
                                     className="cursor-pointer"
@@ -219,10 +259,26 @@ function AccountPage() {
                             className={tab == 2 ? "flex items-center cursor-pointer w-[60px] h-[50px] mr-[50px] border-t-[1px] border-black text-black" : "flex items-center cursor-pointer w-[60px] h-[50px] mr-[50px]"}
                             onClick={() => setTab(2)}
                         >
-                            <div className="text-[15px]">
-                                {SavedIcon}
-                            </div>
-                            <p className="ml-[5px] text-[13px] ">SAVED</p>
+                            { isUser(username) ?
+                                (
+                                  <>
+                                    <div className="text-[15px]">
+                                        {SavedIcon}
+                                    </div>
+                                    <p className="ml-[5px] text-[13px] ">SAVED</p>
+                                  </>
+                                ) :
+                                (
+                                    <>
+                                        <div className="text-[15px]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
+                                            </svg>
+                                        </div>
+                                        <p className="ml-[5px] text-[13px] ">REELS</p>
+                                    </>
+                                ) 
+                            }
                         </div>
 
                         <div 
@@ -240,7 +296,7 @@ function AccountPage() {
                             { isUser(username) ? <Posts userData = { userData } posts ={ posts }/> : <Posts userData = { otherUser } posts ={ posts }/>}
                         </div>
                         <div className={tab == 2 ?"block" : "hidden"}>
-                            { isUser(username) ? <Saved saved = { userData?.saved } userData = { userData } /> : <Saved saved = { otherUser?.saved } userData = { otherUser }/> }
+                            { isUser(username) ? <Saved saved = { userData?.saved } userData = { userData } /> : <Reels saved = { otherUser?.saved } userData = { otherUser }/> }
                         </div>
                         <div className={tab == 3 ?"block" : "hidden"}>
                             <Tagged/>
