494c6666e8f72d12460931dba429065b08513e49
Jin Eu <a11718trannhuy@gmail.com>
2023-04-12 14:29:02 +0700

    fix redux

--- public/redux/store.js
+++ public/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
-import { userReducer, postReducer, replyReducer } from "./reducer"
+import { userReducer, postReducer, commentReducer } from "./reducer"
 import thunk from "redux-thunk"
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -7,7 +7,7 @@ import storage from "redux-persist/lib/storage";
 const reducers = combineReducers({
     userReducer: userReducer,
     postReducer: postReducer,
-    replyReducer: replyReducer,
+    commentReducer: commentReducer,
 })
 
 const persistConfig = {
