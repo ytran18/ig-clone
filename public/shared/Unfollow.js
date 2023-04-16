fc16b09ee3f2b6c446650ca24ef8728a5e9fdefb
vuhung1402 <badao867@gmail.com>
2023-04-16 21:40:17 +0700

    fix some ui

--- public/shared/Unfollow.js
+++ public/shared/Unfollow.js
@@ -19,7 +19,7 @@ function Unfollow({ handleUnfollowPopUp, getFollower, setFollowing, getFollowing
     }
     
     return(
-        <div className="z-50 fixed w-screen h-screen top-0 left-0 bottom-0 right-0 bg-[rgba(19,18,18,0.71)] bg-opacity-90 flex justify-center items-center drop-shadow-2xl shadow-2xl">
+        <div className="z-50 fixed w-screen h-screen top-0 left-0 bottom-0 right-0 bg-[rgba(19,18,18,0.08)] bg-opacity-90 flex justify-center items-center drop-shadow-2xl shadow-2xl">
             <div className=" bg-white flex flex-col justify-between h-[100px] p-3 rounded-lg">
                 <div className=" font-bold h-[60%]">
                     Are you sure to want unfollow
