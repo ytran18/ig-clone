24a240232895ffba1b20565e6230d3c0705762ce
Jin Eu <a11718trannhuy@gmail.com>
2023-04-18 14:28:21 +0700

    following post first

--- public/shared/PostArea.js
+++ public/shared/PostArea.js
@@ -1,16 +1,85 @@
+import { useEffect, useMemo, useState } from "react"
+
+import { useUserPackageHook } from "../redux/hooks"
+
 import Post from "./Post"
 
-function PostArea ({ posts })
+function PostArea ({ posts, notFollowing })
 {
+
+    // user
+    const user = useUserPackageHook()
+
+    const [notFollowingUsers, setNotFollowingUsers] = useState([])
+    const [follow, setFollow] = useState([])
+    const [notFollow, setNotFollow] = useState([])
+
+    useEffect(() => 
+    {
+        notFollowing?.map((item, index) =>
+        {
+            if (!notFollowingUsers.includes(item?.userId))
+            {
+                setNotFollowingUsers(prev => [...prev, item?.userId])
+            }
+        })
+    },[notFollowing, notFollowingUsers])
+
+
+    useEffect(() =>
+    {
+        notFollowingUsers.length > 0 ? 
+        (
+            posts?.map((item, index) =>
+            {
+                const postObject = Object.values(item)
+                postObject?.map((data, index2) =>
+                {
+                    if (notFollowingUsers.includes(data?.userId))
+                    {
+                        if (!notFollow.includes(data))
+                        {
+                            setNotFollow(prev => [...prev, data])
+                        }
+                    }
+                    else {
+                        if (!follow.includes(data))
+                        {
+                            setFollow(prev => [...prev, data])
+                        }
+                    }
+                })
+            })
+        )
+        :
+        (null)
+    },[notFollowingUsers, posts, follow, notFollow])
+
     // display all posts from database
+
+    const renderFollow = useMemo(() =>
+    {
+        return (
+            <div> <Post post={follow}/> </div> 
+        )
+    },[follow])
+
+    const renderNotFollow = useMemo(() =>
+    {
+        return (
+            <div> <Post post={notFollow}/> </div> 
+        )
+    },[notFollow])
+
+    useEffect(() =>
+    {
+        console.log(notFollowingUsers);
+    },[notFollowingUsers])
+
     return (
         <div className="w-full h-full flex flex-col items-center px-2">
-            {
-                posts.map(( item, index ) =>
-                {
-                    return (  <div key={index}> <Post post={item}/> </div> )
-                })
-            }
+            {renderFollow}
+            {renderNotFollow}
         </div>
     )
 }
