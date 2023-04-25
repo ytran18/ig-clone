a36ee20118a02a42044e9451006597f738ec7377
vuhung1402 <badao867@gmail.com>
2023-04-25 22:06:43 +0700

    catch error when user don't login and close the sign in pop up

--- pages/auth/Login.js
+++ pages/auth/Login.js
@@ -97,7 +97,15 @@ function Login() {
                     })
                     router.push(`/`)
                 }
-            } )
+            } ).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            })
     }
 
     return(
