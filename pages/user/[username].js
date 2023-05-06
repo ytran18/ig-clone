4705f5e83f9a7c56bccc4efa5693dac893901d87
vuhung1402 <badao867@gmail.com>
2023-05-06 22:29:07 +0700

    fix some bug

--- pages/user/[username].js
+++ pages/user/[username].js
@@ -338,35 +338,36 @@ function AccountPage() {
                             <p className="ml-[5px] text-[13px] ">POSTS</p>
                         </div>
 
-                        <div 
-                            className={tab == 2 ? "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px] border-t-[1px] border-black text-black" : "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px]"}
-                            onClick={() => setTab(2)}
-                        >
-                            { isUser(username) ?
-                                (
-                                  <>
+                        {
+                            isUser(username) && 
+                            (
+                                <div 
+                                    className={tab == 2 ? "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px] border-t-[1px] border-black text-black" : "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px]"}
+                                    onClick={() => setTab(2)}
+                                >   
                                     <div className="text-[15px]">
                                         {SavedIcon}
                                     </div>
                                     <p className="ml-[5px] text-[13px] ">SAVED</p>
-                                  </>
-                                ) :
-                                (
-                                    <>
-                                        <div className="text-[15px]">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
-                                            </svg>
-                                        </div>
-                                        <p className="ml-[5px] text-[13px] ">REELS</p>
-                                    </>
-                                ) 
-                            }
+                                </div>
+                            )
+                        }
+
+                        <div 
+                            className={tab == 3 ? "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px] border-t-[1px] border-black text-black" : "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] sm:mr-[50px]"}
+                            onClick={() => setTab(3)}
+                        >
+                            <div className="text-[15px]">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
+                                </svg>
+                            </div>
+                            <p className="ml-[5px] text-[13px] ">REELS</p>
                         </div>
 
                         <div 
-                            className={tab ==3 ? "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] border-t-[1px] border-black text-black" : "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px]"}
-                            onClick={() => setTab(3)}
+                            className={tab ==4 ? "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px] border-t-[1px] border-black text-black" : "flex items-center justify-center cursor-pointer w-[30%] sm:w-[60px] h-[50px]"}
+                            onClick={() => setTab(4)}
                         >
                             <div className="text-[15px]">
                                 {TaggedIcon}
@@ -379,9 +380,12 @@ function AccountPage() {
                             { isUser(username) ? <Posts userData = { userData } posts ={ posts } isUser = {isUser} /> : <Posts userData = { otherUser } posts ={ posts } isUser = {isUser} />}
                         </div>
                         <div className={tab == 2 ?"block" : "hidden"}>
-                            { isUser(username) ? <Saved userData = { userData } /> : <Reels userData = { otherUser }/> }
+                            { <Saved userData = { userData } /> }
                         </div>
                         <div className={tab == 3 ?"block" : "hidden"}>
+                            { isUser(username) ? <Reels userData = { userData } posts ={ posts } isUser = { isUser } /> : <Reels userData = { otherUser } posts ={ posts } isUser = {isUser} />}
+                        </div>
+                        <div className={tab == 4 ?"block" : "hidden"}>
                             <Tagged/>
                         </div>
                     </div>
