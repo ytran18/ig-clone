0aebe0fd4abc04ccce5e6160d8a325cdd1deb39e
vuhung1402 <badao867@gmail.com>
2023-05-09 22:26:09 +0700

    almost done UI for messages

---
+++ pages/messages.js
@@ -0,0 +1,20 @@
+//component
+import Chat from "../public/shared/Chat"
+import SideBar from "../public/shared/Sidebar"
+
+function Messages() {
+    return(
+        <div className=" w-screen flex">
+            <div className="">
+                <SideBar/>
+            </div>
+            <div className="w-[80%] flex items-center justify-center bg-[#fafafa]">
+                <div className="bg-white w-full h-[90%] m-8 border-[1px]">
+                    <Chat/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Messages
---
+++ public/shared/ChatContent.js
@@ -0,0 +1,57 @@
+
+// icon/image
+import Image from "next/image"
+import XinSoo from "../icons/xinsoo.jpg"
+import {Phone, VideoCamera, MoreDotIcon, Photo, LoveIcon} from "../icons/icons"
+import { useState } from "react"
+
+function ChatContent() {
+
+    const [send, setSend] = useState(false)
+
+    const handleChange = (e) => {
+        if(e.target.value == ""){
+            setSend(false)
+        }else{
+            setSend(true)
+        }
+    }
+
+    return(
+        <div className="w-full h-full">
+            <div className="h-[10%] border-b-[1px] flex items-center justify-between px-4">
+                <div className="flex items-center">
+                    <Image src={XinSoo} className="max-h-[30px] max-w-[30px] rounded-full mr-3" />
+                    <div className="text-black font-semibold cursor-pointer hover:text-[#777373c6]">xinsooo</div>
+                </div>
+                <div className="flex items-center justify-around w-[150px]">
+                    <div className=" cursor-pointer">{Phone}</div>
+                    <div className=" cursor-pointer">{VideoCamera}</div>
+                    <div className=" cursor-pointer"> {MoreDotIcon} </div>
+                </div>
+            </div>
+            <div className="h-[90%]">
+                <div className="h-[80%]">
+                    <div className=" my-4 mx-4 w-[250px] h-auto rounded-xl border-[1px] px-3 py-2">
+                        hello
+                    </div>
+                    <div className="my-4 mx-4 w-[250px] h-auto rounded-xl border-[1px] float-right px-3 py-2 flex flex-row-reverse">Homnay toi buon</div>
+                </div>
+                <div className="h-[20%] flex items-center justify-center">
+                    <div className=" rounded-3xl border-[1px] w-[90%] h-[50%] flex items-center justify-between px-6">
+                        <input onChange={handleChange} className=" outline-none w-[90%]" placeholder="Messages...."/>
+                        <div className={send ? "hidden" : "flex w-[10%] justify-between"}>
+                            <div>{Photo}</div>
+                            <div> {LoveIcon} </div>
+                        </div>
+                        <div className={send ? "w-[10%] text-blue-400" : "hidden"}>
+                            Send
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatContent
