9c1edecd94ca6fa6bc5cd76b462f87bc33b8c319
vuhung1402 <badao867@gmail.com>
2023-05-20 22:22:18 +0700

    user can find other users and can chat with them

--- public/shared/NewMessSearchResult.js
+++ public/shared/NewMessSearchResult.js
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import Xinsoo from "../icons/xinsoo.jpg"
 
-function NewMessSearchResult({item}){
+function NewMessSearchResult({item, setUserChat, setSearch}){
+
+    const handleClickResult = () => {
+        setUserChat(item?.userId)
+        setSearch(item?.username)
+    }
+
     return(
-        <div className="p-4 flex items-center gap-3 cursor-pointer hover:bg-[rgb(250,250,250)]">
-            {/* <Image className="h-[50px] w-[50px] rounded-full" src={Xinsoo}/> */}
-
+        <div onClick={handleClickResult} className="p-4 flex items-center gap-3 cursor-pointer hover:bg-[rgb(250,250,250)]">
             <img src={item?.avatar} className="h-[50px] w-[50px] rounded-full" />
-
             <div>
                 <div className=" font-medium">{item?.username}</div>
                 <div className=" text-[#7b7b7b] text-[15px]">{item?.name}</div>
--- public/shared/NewMessage.js
+++ public/shared/NewMessage.js
@@ -3,18 +3,58 @@ import {CloseIcon} from "../icons/icons"
 import Xinsoo from "../icons/xinsoo.jpg"
 
 // firebase
-import { auth,db } from "../../src/firebase"
+import { auth,db,fireStore } from "../../src/firebase"
 import { endAt, onValue, orderByChild, query, ref, startAt } from "firebase/database"
+import { collection, addDoc,serverTimestamp, doc, setDoc,where, getDocs, onSnapshot, query as query1, orderBy, updateDoc } from 'firebase/firestore'
+
 
 //hooks
-import { useState } from "react"
+import { useState,useEffect } from "react"
+import { useRouter } from "next/router"
 
+//component
 import NewMessSearchResult from "./NewMessSearchResult"
+
+import {useUserPackageHook} from "../redux/hooks"
+
+const uuid = require("uuid")
 
 function NewMessage({handleClose}) {
 
     const [search, setSearch] = useState("")
     const [searchResult, setSearchResult] = useState([])
+    const [userMess, setUserMess] = useState([])
+
+    //user who main user want to chat 
+    const [userChat, setUserChat] = useState("")
+
+    const user = useUserPackageHook()
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const q = query1(collection(fireStore, `${user?.userId}`))
+        onSnapshot(q, (snapshot) => {
+            let userMess1 = []
+            snapshot.forEach((doc) => {
+                userMess1.push(doc.data().userId)
+            })
+            setUserMess(userMess1)
+        })
+    },[])
+
+    //check if user has messaged with this user
+    const checkUserMess = (userId) => {
+        let isUser = false
+        userMess?.forEach((id) => {
+            if(id === userId){
+                isUser = true
+            }
+        })
+        return isUser
+    }
+
+    console.log(userMess)
 
     const handleChangeSearch = (e) => {
         setSearch(e.target.value)
@@ -29,13 +69,39 @@ function NewMessage({handleClose}) {
         })
     }
 
+    const handleChat = async () => {
+        const id = uuid.v4()
+
+        if(checkUserMess(userChat)){
+            router.push("/messages")
+            handleCloseNewMess()
+        }
+        else{
+            await setDoc(doc(fireStore, `${user?.userId}`, id), {
+                messId: id,
+                userId: userChat
+            }, {merge: true})
+            await setDoc(doc(fireStore, `${userChat}`, id), {
+                messId: id,
+                userId: user?.userId
+            }, {merge: true})
+            router.push("/messages")
+            handleCloseNewMess()
+        }
+    }
+
     const handleCloseNewMess = () => {
         handleClose()
         setSearch("")
         setSearchResult([])
+        setUserChat("")
     }
 
     console.log(searchResult)
+
+    console.log("user chat: ", userChat)
+
+    console.log("Check User Mess:", checkUserMess(userChat))
 
     return(
         <div className="fixed w-screen h-screen top-0 left-0 bottom-0 right-0 bg-[rgb(89,89,89)] bg-opacity-90 flex justify-center items-center drop-shadow-2xl shadow-2xl">
@@ -51,12 +117,12 @@ function NewMessage({handleClose}) {
                 <div className="h-[50%] overflow-y-scroll">
                     {searchResult?.map((item, index) => {
                         return(
-                            <div key={index} > <NewMessSearchResult item={item} /> </div>
+                            <div key={index} > <NewMessSearchResult item={item} setUserChat = {setUserChat} setSearch = {setSearch} /> </div>
                         )
                     })}
                 </div>
                 <div className="h-[20%]">
-                    <div className="h-[50%] mx-3 my-5 flex items-center justify-center bg-blue-500 rounded-2xl text-white font-semibold cursor-pointer">Chat</div>
+                    <div onClick={handleChat} className="h-[50%] mx-3 my-5 flex items-center justify-center bg-blue-500 rounded-2xl text-white font-semibold cursor-pointer">Chat</div>
                 </div>
             </div>
         </div>
