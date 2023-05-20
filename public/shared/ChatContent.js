e34d7d8bfb6f8c8a8f9dc9970ae3dd431727bb39
vuhung1402 <badao867@gmail.com>
2023-05-20 17:22:15 +0700

    fix something for logic

--- public/shared/ChatContent.js
+++ public/shared/ChatContent.js
@@ -1,7 +1,7 @@
 
 // icon/image
 import Image from "next/image"
-import XinSoo from "../icons/xinsoo.jpg"
+import MessageIcon from "../icons/images.png"
 import {Phone, VideoCamera, MoreDotIcon, Photo, LoveIcon} from "../icons/icons"
 import { useEffect, useState, useRef } from "react"
 
@@ -12,7 +12,7 @@ import { collection, addDoc,serverTimestamp, doc, setDoc,where, getDocs, onSnaps
 //redux 
 import {useUserPackageHook} from "../redux/hooks"
 
-function ChatContent({messId, otherUser}) {
+function ChatContent({handleNewMessage, messId, otherUser}) {
 
     const [send, setSend] = useState(false)
     const [messages, setMessages] = useState([])
@@ -63,61 +63,77 @@ function ChatContent({messId, otherUser}) {
     }
 
     return(
-        <div className="w-full h-full">
-            <div className="h-[10%] border-b-[1px] flex items-center justify-between px-4">
-                <div className="flex items-center">
-                    {/* <Image src={XinSoo} className="max-h-[30px] max-w-[30px] rounded-full mr-3" /> */}
-                    <img src={otherUser?.avatar} className="max-h-[30px] max-w-[30px] rounded-full mr-3" />
-                    <div className="text-black font-semibold cursor-pointer hover:text-[#777373c6]">{otherUser?.username}</div>
-                </div>
-                <div className="flex items-center justify-around w-[150px]">
-                    <div className=" cursor-pointer">{Phone}</div>
-                    <div className=" cursor-pointer">{VideoCamera}</div>
-                    <div className=" cursor-pointer"> {MoreDotIcon} </div>
-                </div>
-            </div>
-            <div className="h-[90%]">
-                <div className="h-[80%] overflow-y-scroll scrollbar-hide">
-                    {
-                        messages?.map((message) => {
-                            if(user?.userId == message?.userId){
-                                return(
-                                    <div className="flex flex-row-reverse my-2 mx-2">
-                                        <div className="w-fit h-auto rounded-xl border-[1px] px-3 py-2">
-                                            <div>{message?.text}</div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                            else{
-                                return(
-                                    <div className=" my-4 mx-4 w-fit h-auto rounded-xl border-[1px] px-3 py-2">
-                                        {message?.text}
-                                    </div>
-                                )
-                            }
-                        })   
-                    }
-                    <div ref={messRef}></div>
-                </div>
-                <div className="h-[20%] flex items-center justify-center">
-                    <div className=" rounded-3xl border-[1px] w-[90%] h-[50%] flex items-center justify-between px-6">
-                        <input 
-                            onChange={handleChange} 
-                            className=" outline-none w-[90%]" 
-                            placeholder="Messages...."
-                            value={newMess}
-                            onKeyDown={handleSend}/>
-                        <div className={send ? "hidden" : "flex w-[10%] justify-between"}>
-                            <div>{Photo}</div>
-                            <div> {LoveIcon} </div>
+        <div className="w-full h-full flex items-center justify-center">
+            {
+                messId === "" ?
+                (
+                    <div className="flex flex-col items-center">
+                        <Image src={MessageIcon} />
+                        <div className=" font-medium text-[20px]">Your messages</div>
+                        <div>Send private photos and messages to a friend or group</div>
+                        <div onClick={handleNewMessage} className="p-4 bg-blue-600 rounded-xl text-white font-semibold cursor-pointer">Send message</div>
+                    </div> 
+                ) :
+
+                (
+                    <div className="w-full h-full">
+                        <div className="h-[10%] border-b-[1px] flex items-center justify-between px-4">
+                            <div className="flex items-center">
+                                <img src={otherUser?.avatar} className="max-h-[30px] max-w-[30px] rounded-full mr-3" />
+                                <div className="text-black font-semibold cursor-pointer hover:text-[#777373c6]">{otherUser?.username}</div>
+                            </div>
+                            <div className="flex items-center justify-around w-[150px]">
+                                <div className=" cursor-pointer">{Phone}</div>
+                                <div className=" cursor-pointer">{VideoCamera}</div>
+                                <div className=" cursor-pointer"> {MoreDotIcon} </div>
+                            </div>
                         </div>
-                        <div className={send ? "w-[10%] text-blue-400" : "hidden"}>
-                            Send
+                        <div className="h-[90%]">
+                            <div className="h-[80%] overflow-y-scroll scrollbar-hide">
+                                {
+                                    messages?.map((message) => {
+                                        if(user?.userId == message?.userId){
+                                            return(
+                                                <div className="flex flex-row-reverse my-2 mx-2">
+                                                    <div className="w-fit h-auto rounded-xl border-[1px] px-3 py-2">
+                                                        <div>{message?.text}</div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+                                        else{
+                                            return(
+                                                <div className=" my-4 mx-4 w-fit h-auto rounded-xl border-[1px] px-3 py-2">
+                                                    {message?.text}
+                                                </div>
+                                            )
+                                        }
+                                    })   
+                                }
+                                <div ref={messRef}></div>
+                            </div>
+                            <div className="h-[20%] flex items-center justify-center">
+                                <div className=" rounded-3xl border-[1px] w-[90%] h-[50%] flex items-center justify-between px-6">
+                                    <input 
+                                        onChange={handleChange} 
+                                        className=" outline-none w-[90%]" 
+                                        placeholder="Messages...."
+                                        value={newMess}
+                                        onKeyDown={handleSend}/>
+                                    <div className={send ? "hidden" : "flex w-[10%] justify-between"}>
+                                        <div>{Photo}</div>
+                                        <div> {LoveIcon} </div>
+                                    </div>
+                                    <div className={send ? "w-[10%] text-blue-400" : "hidden"}>
+                                        Send
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                )
+
+            }
         </div>
     )
 }
