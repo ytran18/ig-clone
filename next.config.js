8b2c0977d0ff493f1898ce0ee4a23b0d7a3a43de
vuhung1402 <badao867@gmail.com>
2023-05-24 20:04:18 +0700

    fix something back

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
